5a6f08a4fe30a53ef0cbfc2b6020e47f4435633b
shahtiya <shahtiya02@gmail.com>
2026-06-11 10:48:43 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+numbers = [10, 25, 5, 40, 15]
+
+highest = max(numbers)
+lowest = min(numbers)
+average = sum(numbers) / len(numbers)
+
+print("Numbers:", numbers)
+print("Highest:", highest)
+print("Lowest:", lowest)
+print("Average:", average)
